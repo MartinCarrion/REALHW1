0da46f10216cce66dfeda7390cfba2932b4dc6da
MartinACarrion <martin.a.carrion-1@ou.edu>
2019-01-30 14:33:16 -0600

    PT1Finished

---
+++ PT1LOOPS/PT1LOOPS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT1LOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LOOPS
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i > 0)
+                {
+                    Console.WriteLine("Input 1st Exam Score");
+                    int ExamScore1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Input 2nd Exam Score");
+                    int ExamScore2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Input 3rd Exam Score");
+                    int ExamScore3 = Convert.ToInt32(Console.ReadLine());
+                    int AverageExamScores = ExamScore1 / 3 + ExamScore2 / 3 + ExamScore3 / 3;
+                    Console.WriteLine("Your Average Exam Score is: " + AverageExamScores + "%");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Would You Like to Enter New Exam Scores? y/n");
+                    string YN = Console.ReadLine();
+                    if (YN == "n")
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    
+
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
